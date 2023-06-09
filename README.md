5e55fdd83aa6a488c0f83b55c2a7ee9d4d4d00ea
Mikulas <mikulas.stastny@bringauto.com>
2023-06-09 08:38:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+WIP
+
+# Generating Protobuf files
+
+TODO
+
+# Generating zip library usable with Arduino IDE
+
+In root directory use:
+
+```bash
+python zip_script
+```
+
+TODO
---
+++ source/internal_client.cpp
@@ -0,0 +1,17 @@
+#include <internal_client.h>
+
+int init_connection(void **context, const char* const ipv4_address, unsigned port, const struct device_identification device){
+
+}
+
+int destroy_connection(void **context){
+
+}
+
+int send_status(void *context, const struct buffer status, unsigned timeout){
+
+}
+
+int get_command(void *context, struct buffer* command){
+
+}
