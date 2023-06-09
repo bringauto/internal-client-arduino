cbc34fedff293bef6bff85b7535d3c5806a65857
Mikulas <mikulas.stastny@bringauto.com>
2023-06-09 23:20:24 +0200

    Change zip_script to include all necessary files

--- zip_script.py
+++ zip_script.py
@@ -1,11 +1,12 @@
 import zipfile as zf
+import glob
+import re
+
+def remove_path(filename : str):
+    return re.sub('.*/', '', filename)
 
 with zf.ZipFile("internal-client-arduino.zip", "w") as f:
-    f.write("include/internal_client.h", "internal-client-arduino/internal_client.h")
-    f.write("include/device_management.h", "internal-client-arduino/device_management.h")
-    f.write("include/general_error_codes.h", "internal-client-arduino/general_error_codes..h")
-    f.write("include/ic_error_codes.h", "internal-client-arduino/ic_error_codes.h")
-    f.write("include/memory_management.h", "internal-client-arduino/memory_management.h")
-    f.write("source/internal_client.cpp", "internal-client-arduino/internal_client.cpp")
-    f.write("source/protobuf/compiled/InternalProtocol.pb.h", "internal-client-arduino/InternalProtocol.pb.h")
-    f.write("source/protobuf/compiled/InternalProtocol.pb.c", "internal-client-arduino/InternalProtocol.pb.c")    
+    for file in glob.glob("include/*.h") + glob.glob("include/**/*.hpp", recursive = True) \
+                + glob.glob("source/**/*.cpp", recursive = True) + glob.glob("source/**/*.c", recursive = True) \
+                + glob.glob("source/**/*.h", recursive = True):
+        f.write(file, "internal-client-arduino/" + remove_path(file))
